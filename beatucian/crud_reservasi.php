bb75c1cb4abb06993fe55a86590d4074cb7377b2
lakuna2 <auliaadeputri2003@gmail.com>
2023-05-08 17:59:04 +0800

    tambah table beatucian

---
+++ beatucian/crud_reservasi.php
@@ -0,0 +1,195 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CRUD Treatments</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../lib/animate/animate.min.css" rel="stylesheet">
+    <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../css/style.css" rel="stylesheet">
+
+     <!-- data tables -->
+     <link rel="stylesheet" type="text/css" href="assets/datatables/datatables.min.css">
+     <script type="text/javascript" src="assets/datatables/datatables.min.js"></script>
+
+     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css"> -->
+     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+     
+
+     <style type="text/css">
+    
+        table {
+        border: solid 1px #DDEEEE;
+        border-collapse: collapse;
+        border-spacing: 0;
+        width: 100%;
+        margin: 10px auto 80px auto;
+        }
+        table thead th {
+            background-color: #DDEFEF;
+            border: solid 1px #DDEEEE;
+            color: #336B6B;
+            padding: 10px;
+            text-align: left;
+            text-shadow: 1px 1px 1px #fff;
+            text-decoration: none;
+        }
+        table tbody td {
+            border: solid 1px #DDEEEE;
+            color: #333;
+            padding: 10px;
+            text-shadow: 1px 1px 1px #fff;
+        }
+
+    </style>
+    
+</head>
+
+<body>
+
+    <!-- Navbar Start -->
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
+            <a href="beatucian.php" class="navbar-brand ml-lg-3">
+                <h1 class="m-0 text-primary"><span class="text-dark">AYL</span> Beauty Spa</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
+                <div class="navbar-nav m-auto py-0">
+                    <a href="beatucian.php" class="nav-item nav-link active">Home</a>
+                    <a href="crud_reservasi.php" class="nav-item nav-link">Reservation</a>
+                </div>
+                <a href="login.php" class="btn btn-primary d-none d-lg-block">Log Out</a>
+            </div>
+        </nav>
+    </div>
+    <!-- Navbar End -->
+
+    <!-- TABLE CRUD Start -->
+
+    <center><h1>Data Reservasi</h1><center>
+    <div class="container">
+        <table id='example'>
+        <thead>
+            <tr>
+            <th>ID</th>
+            <th>Tanggal</th>
+            <th>Waktu</th>
+            <th>Nama</th>
+            <th>No HP</th>
+            <th>Treatment</th>
+            <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
+        $query = "SELECT * FROM reservasi ORDER BY kode_reservasi ASC";
+        $result = mysqli_query($koneksi, $query);
+        //mengecek apakah ada error ketika menjalankan query
+        if(!$result){
+            die ("Query Error: ".mysqli_error($koneksi).
+            " - ".mysqli_error($koneksi));
+        }
+
+        //buat perulangan untuk element tabel dari data mahasiswa
+        $no = 1; //variabel untuk membuat nomor urut
+        // hasil query akan disimpan dalam variabel $data dalam bentuk array
+        // kemudian dicetak dengan perulangan while
+        while($row = mysqli_fetch_assoc($result))
+        {
+        ?>
+        <tr>
+            <td><?php echo $no; ?></td>
+            <td><?php echo $row['Tanggal']; ?></td>
+            <td><?php echo $row['Waktu']; ?></td>
+            <td><?php echo $row['Nama']; ?></td>
+            <td><?php echo $row['No HP']; ?></td>
+            <td><?php echo $row['Treatment']; ?></td>
+            <td>
+                <!-- <a href="edit_lynn.php?id=<?php echo $row['id']; ?>">Edit</a> | -->
+                <a href="proses_hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a> |
+                <a href="selesai.php?id=<?php echo $row['id']; ?>">Selesai</a>
+            </td>
+        </tr>
+            
+        <?php
+            $no++; //untuk nomor urut terus bertambah 1
+        }
+        ?>
+        </tbody>
+        </table>
+    </div>
+    <!-- TABLE CRUD End -->
+
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light border-top py-4" style="border-color: rgba(256, 256, 256, .15) !important;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-left mb-3 mb-md-0">
+                    <p class="m-0 text-white">&copy; <a href="#">AYLBeautySpa</a>. All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="../lib/easing/easing.min.js"></script>
+    <script src="../lib/waypoints/waypoints.min.js"></script>
+    <script src="../lib/counterup/counterup.min.js"></script>
+    <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="../lib/tempusdominus/js/moment.min.js"></script>
+    <script src="../lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="../lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="../js/jqBootstrapValidation.min.js"></script>
+    <script src="../js/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="../js/main.js"></script>
+
+    <script>
+        $(document).ready(function () {
+    $('#example').DataTable();
+});
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+</body>
+
+</html>
